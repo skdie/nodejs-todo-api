8cd20be1b2d3e5be38f1f583df71a4b9ef99982d
skdie <galabpokh@gmail.com>
2018-03-22 11:49:24 -0700

    setting up email in database and sending email

--- server/server.js
+++ server/server.js
@@ -1,22 +1,34 @@
 var mongoose = require('mongoose');
+var nodemailer = require('nodemailer');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-
-
-
-var Todo = mongoose.model('Todo',{
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAT: {
-        type: Number
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'galabpokh@gmail.com',
+      pass: ''
     }
-});
+  });
+  
+
+
+// var Todo = mongoose.model('Todo',{
+//     text: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     completedAT: {
+//         type: Number,
+//         default: null
+//     }
+// });
 
 // var newTodo = new Todo({
 //     text: 'Go to quiz'
@@ -29,14 +41,51 @@ var Todo = mongoose.model('Todo',{
     
 // });
 
-var newTodo = new Todo({
-    text: 'Get Up Morning',
-    completed: true,
-    completedAT: 3453
-});
-newTodo.save().then((doc) =>{
-    console.log('Saved todo',doc)
-},(err) =>{
-    console.log('Unable to save ',err);
+// var newTodo = new Todo({
+//     text: 'Get Up Morning',
+//     completed: true,
+//     completedAT: 3453
+// });
+// newTodo.save().then((doc) =>{
+//     console.log(JSON.stringify(doc, undefined, 2));
+// },(err) =>{
+//     console.log('Unable to save ',err);
+// });
+
+
+// var newTodo = new Todo({text: '  This is a test. '});
+// newTodo.save().then((doc) =>{
+//     console.log(JSON.stringify(doc, undefined, 2));
+// },(err) =>{
+//     console.log('Unable to save ',err);
+// });
+
+
+var User = mongoose.model('User',{
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
 });
 
+var newUser = new User({email: 'galabpokh@gmail.com'});
+newUser.save().then((doc) =>{
+    console.log(JSON.stringify(doc, undefined, 2));
+    var mailOptions = {
+        from: '149maxwellgalab@gmail.com',
+        to: 'galabpokh@gmail.com',
+        subject: 'Subject',
+        text: 'Sending from node js'
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+},(err) =>{
+    console.log('Unable to create User',err);
+});
