9b67ecfba07fef4009aab2daa4a54fa33d437e5c
skdie <galabpokh@gmail.com>
2018-03-22 11:08:43 -0700

    mongoose simple add data

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 
 
@@ -16,5 +16,27 @@ var Todo = mongoose.model('Todo',{
     completedAT: {
         type: Number
     }
+});
+
+// var newTodo = new Todo({
+//     text: 'Go to quiz'
+// });
+
+// newTodo.save().then((doc) =>{
+//     console.log('Saved todo',doc)
+// },(e) =>{
+//     console.log('Unable to save');
+    
+// });
+
+var newTodo = new Todo({
+    text: 'Get Up Morning',
+    completed: true,
+    completedAT: 3453
+});
+newTodo.save().then((doc) =>{
+    console.log('Saved todo',doc)
+},(err) =>{
+    console.log('Unable to save ',err);
 });
 
